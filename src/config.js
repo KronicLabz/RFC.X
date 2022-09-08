b63c7dc1eadfd9f5b735effb1d1599cd7d261b46
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-09-08 12:50:01 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const NFTCONTRACT = "0x252c4B998fC3B3Dd6e13F6868477f37a855f9e6E";
-export const STAKINGCONTRACT = "0x15B76B01cA549473Fd3844f2Af97Be0f8f227BcC";
-export const etherscanapi = "https://api.etherscan.io/YXH8ERRHWCY82R5VZ59R39SZU81MUPSEHY/";
-export const moralisapi = "https://deep-index.moralis.io/api/v2/y8oGt2GEKCxEnMVKfpRVTlZjBPK0Cay9yxZxu0DScdd4In4jwdlJNH5cbU9pJFlE/";
-export const nftpng = "https://ipfs.io/ipfs/QmQYdN9FnTqGLxDDztbRzBNFUCCV9ffMdqT7Mn2V8H4Uth/";
+export const NFTCONTRACT=0x252c4B998fC3B3Dd6e13F6868477f37a855f9e6E
+export const STAKINGCONTRACT=0x15B76B01cA549473Fd3844f2Af97Be0f8f227BcC
+export const etherscanapi= https://api.etherscan.io/YXH8ERRHWCY82R5VZ59R39SZU81MUPSEHY/
+export const moralisapi=https://deep-index.moralis.io/api/v2/
+export const nftpng=https://ipfs.io/ipfs/QmQYdN9FnTqGLxDDztbRzBNFUCCV9ffMdqT7Mn2V8H4Uth/
