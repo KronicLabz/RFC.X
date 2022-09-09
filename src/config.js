49f5c222c6005dc0438d55d9edb9ae11a5144d8d
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-09-08 21:22:47 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
-export const NFTCONTRACT=0x252c4B998fC3B3Dd6e13F6868477f37a855f9e6E
-export const STAKINGCONTRACT=0x15B76B01cA549473Fd3844f2Af97Be0f8f227BcC
-export const etherscanapi= https://api.etherscan.io/YXH8ERRHWCY82R5VZ59R39SZU81MUPSEHY/
-export const moralisapi=https://deep-index.moralis.io/api/v2/
-export const nftpng=https://ipfs.io/ipfs/QmQYdN9FnTqGLxDDztbRzBNFUCCV9ffMdqT7Mn2V8H4Uth/
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+export const NFTCONTRACT = "0x252c4B998fC3B3Dd6e13F6868477f37a855f9e6E";
+export const STAKINGCONTRACT = "0x15B76B01cA549473Fd3844f2Af97Be0f8f227BcC";
+export const etherscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng =
+  "https://ipfs.io/ipfs/QmQYdN9FnTqGLxDDztbRzBNFUCCV9ffMdqT7Mn2V8H4Uth/";
+export const Web3Alc = createAlchemyWeb3(
+  "https://eth-mainnet.g.alchemy.com/v2/LImfUcM87aQ4q0LLq3x798zzhuleAAvw"
+);
